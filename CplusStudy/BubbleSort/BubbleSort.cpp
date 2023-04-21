da2a6686939c1d6d12ad62ff58538a28bb43981c
青山研人 <106291245+KentoAoyama@users.noreply.github.com>
2023-04-21 16:07:43 +0900

    Test

---
+++ CplusStudy/BubbleSort/BubbleSort.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+	int array[] = { 2, 1, 3, 5, 4, 6 };
+
+	for (int i = 0; i < sizeof(array) / sizeof(int) - 1; i++) {
+		for (int j = i + 1; j < sizeof(array) / sizeof(int); j++) {
+			if (array[i] < array[j]) continue;
+
+			swap(array[i], array[j]);
+			//int a = array[i];
+			//array[i] = array[j];
+			//array[j] = a;
+		}
+	}
+
+	for (int k = 0; k < sizeof(array) / sizeof(int); k++)
+	{
+		cout << array[k] << endl;
+	}
+}
---
+++ CplusStudy/Various/Various.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+	int num;
+	cin >> num;
+	vector<int> vnums;
+
+	for (int i = 0; i < num; i++) {
+		int data;
+		cin >> data;
+		vnums.push_back(data);
+	}
+
+	sort(vnums.begin(), vnums.end());
+
+	for (auto n : vnums) {
+		cout << n << endl;
+	}
+}

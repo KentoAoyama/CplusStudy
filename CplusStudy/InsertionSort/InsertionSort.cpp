48d3a6e093c1acf70de6285778c775e6f04d360e
青山研人 <106291245+KentoAoyama@users.noreply.github.com>
2023-04-26 16:51:22 +0900

    インサートソートを勉強

---
+++ CplusStudy/InsertionSort/InsertionSort.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+	vector<int> nums = { 7, 1, 3, 4, 2 };
+
+	for (int i = 1; i < nums.size(); i++) {
+		int count = 0;
+		for (int j = i - 1; j >= 0; j--) {
+			if (nums[i - count] < nums[j]) {
+				swap(nums[i - count], nums[j]);
+				count++;
+			}
+			else {
+				break;
+			}
+		}
+	}
+
+	for (auto n : nums) {
+		cout << n << endl;
+	}
+}

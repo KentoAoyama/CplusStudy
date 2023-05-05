fe7f0fa2b85796613503ddf7c2d096c9ada50c03
青山研人 <106291245+KentoAoyama@users.noreply.github.com>
2023-05-05 23:27:36 +0900

    [update]paizaでやったものを追加　ソートとマップ

--- CplusStudy/CplusStudy/CplusStudy.cpp
+++ CplusStudy/CplusStudy/CplusStudy.cpp
@@ -1,20 +1,30 @@
-﻿// CplusStudy.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
-//
+﻿#include <iostream>
+#include <map>
+#include <vector>
+#include <algorithm>
 
-#include <iostream>
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+int main() {
+    int n;
+    cin >> n;
+    map<string, int> maps;
+    for (string s; n-- && cin >> s; maps[s] = 0);
+
+    cin >> n;
+    for (string s; n-- && cin >> s;) {
+        int num;
+        cin >> num;
+        maps[s] += num;
+    }
+
+    vector<pair<string, int>> pairs;
+    for (auto m : maps) {
+        pairs.push_back(pair<string, int>(m.first, m.second));
+    }
+    sort(pairs.begin(), pairs.end());
+
+    for (auto p : pairs) {
+        cout << p.second << endl;
+    }
 }
-
-// プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
-// プログラムのデバッグ: F5 または [デバッグ] > [デバッグの開始] メニュー
-
-// 作業を開始するためのヒント: 
-//    1. ソリューション エクスプローラー ウィンドウを使用してファイルを追加/管理します 
-//   2. チーム エクスプローラー ウィンドウを使用してソース管理に接続します
-//   3. 出力ウィンドウを使用して、ビルド出力とその他のメッセージを表示します
-//   4. エラー一覧ウィンドウを使用してエラーを表示します
-//   5. [プロジェクト] > [新しい項目の追加] と移動して新しいコード ファイルを作成するか、[プロジェクト] > [既存の項目の追加] と移動して既存のコード ファイルをプロジェクトに追加します
-//   6. 後ほどこのプロジェクトを再び開く場合、[ファイル] > [開く] > [プロジェクト] と移動して .sln ファイルを選択します
